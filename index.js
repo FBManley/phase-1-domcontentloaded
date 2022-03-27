187918d5814827f1121b71e06d27a62fc9edaabb
Jacob Manley <jbmanley45@gmail.com>
2022-03-27 09:16:52 -0700

    commit tree 163f139cbba495fd6170aad2632bf7385d421665

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(text) {
+     console.log("The DOM has loaded");   });
+  
+
+//   First, set up a DOMContentLoaded event listener to detect when the HTML page has loaded and the document is ready to be manipulated.
+// Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+
+// function addingEventListener(){
+//     const input = document.getElementById('#text');
+
+//     function clickAlert() {
+//       alert('This is really cool!');
+//     }
+    
+//     input.addEventListener('click', clickAlert); 
+// }
+
+// addingEventListener();
+// clickAlert();
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log("The DOM has loaded");
+// });
+
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
